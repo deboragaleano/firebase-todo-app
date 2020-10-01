f5591fab7af0330931e55102cacd42168fb5c671
deboragaleano <dgaleanoservian@gmail.com>
2020-10-01 17:21:32 +0200

    add new todo to db

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import firebase from 'firebase'; 
 import './App.css';
 import Todo from './Todo'; 
 import { Button, FormControl, InputLabel, Input, Container } from '@material-ui/core';
@@ -9,11 +10,13 @@ function App() {
   const [input, setInput] = useState('')
 
   useEffect(() => {
-    db.collection('todos').onSnapshot(snapshot => {
+    // orderBy here takes to params, one is the key to which you want to sort it by
+    // the second is ascending or descending 
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
       let todos = snapshot.docs.map(doc => doc.data())
       setTodos(todos); 
     })
-  }, [todos])
+  }, [])
 
   const handleChange = (e) => {
     setInput(e.target.value)
@@ -21,13 +24,15 @@ function App() {
 
   const addTodo = (e) => {
     e.preventDefault(); 
-    db.collection('todos').doc(input).set({
-      text: input
+
+    //this adds to the db automatically we don't need to setTodos again
+    // and it lists out alphabetically :) 
+    db.collection('todos').add({
+      text: input,
+      //when the data is stored, it will save the timestamp of that server 
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
     })
-    .then(newTodo => {
-      setTodos([...todos, newTodo]); 
-      setInput(''); 
-    })
+    setInput('') //clearing the input locally
   }
 
   return (
